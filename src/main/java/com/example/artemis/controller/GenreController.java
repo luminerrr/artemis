04ea8a720ce7ee82105cdef2b96ffcb4c1a2d4ea
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-09 19:19:36 +0700

    implement controller

--- src/main/java/com/example/artemis/controller/GenreController.java
+++ src/main/java/com/example/artemis/controller/GenreController.java
@@ -10,10 +10,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -26,15 +32,31 @@ public class GenreController {
   // GET /
   @GetMapping()
   public ResponseEntity<List<GenreEntity>> getAllGenres() {
-
     return genreService.getAllGenres();
   }
 
-
   // GET /id
-
+  @GetMapping("/{id}")
+  public ResponseEntity<GenreEntity> getGenreById(@PathVariable Long id) {
+      return genreService.getGenreById(id);
+  }
+  
 
   // POST /
-
+  @PostMapping
+  public ResponseEntity<GenreEntity> createGenre(@RequestBody GenreEntity entity) {
+      return genreService.saveGenre(entity);
+  }
+  
   // PUT /id
+  @PutMapping("/{id}")
+  public ResponseEntity<GenreEntity> updateGenre(@PathVariable Long id, @RequestBody GenreEntity entity) { 
+      return genreService.updateGenre(id, entity);
+  }
+
+  // DELETE /id
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteGenre(@PathVariable Long id) {
+    return genreService.deleteGenre(id);
+  }
 }
