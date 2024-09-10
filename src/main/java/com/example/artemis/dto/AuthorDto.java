f5cf43287eb22620897f11ff6740aac0a94996cf
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-10 09:17:42 +0700

    add author dto

---
+++ src/main/java/com/example/artemis/dto/AuthorDto.java
@@ -0,0 +1,17 @@
+package com.example.artemis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class AuthorDto {
+  private Long id;
+  private String name;
+  private String bio;
+}
