36c41af921751dc4260949e40d3dc337a8489789
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-10 10:49:13 +0700

    change book entity join fetch

--- src/main/java/com/example/artemis/entity/BookEntity.java
+++ src/main/java/com/example/artemis/entity/BookEntity.java
@@ -5,15 +5,16 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Table
+@Table(name = "books")
 @Setter
 @Getter
 
-public class BookEntity {
+public class BookEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,12 +28,12 @@ public class BookEntity {
     @JsonProperty("isbn")
     private String isbn;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id", referencedColumnName = "id", nullable = false)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
     private AuthorEntity author;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "genre_id", referencedColumnName = "id", nullable = false)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "genre_id", referencedColumnName = "id")
     private GenreEntity genre;
 
     @Column(name = "published_date", nullable = false)
